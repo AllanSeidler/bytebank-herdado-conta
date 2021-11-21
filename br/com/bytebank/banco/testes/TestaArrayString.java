ce1c8f96be03b69e26241e9e1bf2f387c1ad7c0b
AllanSeidler <91341721+AllanSeidler@users.noreply.github.com>
2021-11-20 23:40:08 -0300

    Upload de arquivos.
    Espero que esteja tudo aí :D.
    Estou sem tempo para conferir agora.

---
+++ br/com/bytebank/banco/testes/TestaArrayString.java
@@ -0,0 +1,14 @@
+package br.com.bytebank.banco.testes;
+
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
+public class TestaArrayString {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
---
+++ br/com/bytebank/banco/testes/TesteArrayDePrimitivo.java
@@ -0,0 +1,21 @@
+package br.com.bytebank.banco.testes;
+
+public class TesteArrayDePrimitivo {
+
+	//Array
+	public static void main(String[] args) {
+		
+		int[] idades = new int[5];//inicializa o array com 0
+		
+		for(int i = 0; i < idades.length; i++) {
+			idades[i] = i * i;
+			
+		}
+		
+		for(int i = 0; i < idades.length; i++) {
+			System.out.println(idades[i]);
+			
+		}
+		
+	}
+}
